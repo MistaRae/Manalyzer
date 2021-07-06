14b1c28da0dbb769fe3e7ff32e3e64b2a0100dab
MistaRae <itsamistarae@gmail.com>
2021-07-07 00:14:34 +0100

    containers styled & placeholders added to all decks screen

--- client/components/all_decks/DeckSnapshot.js
+++ client/components/all_decks/DeckSnapshot.js
@@ -18,18 +18,27 @@ const DeckSnapshot = ({deck, navigation: { navigate }}) => {
                 const id = deck.id
                 navigate('SpecificDeck', {deck_id: id, deck});
             }}>
-            <View>
-                <View>
-            <Text style={styles.h1}>
-                DECK NAME: {deck.name}
-            </Text>
-            <Text style={styles.h2}>
-                CARD COUNT: {cardCount}
-            </Text>
-            </View>
-            <View>
-                <Image/>
-            </View>
+            <View
+            style={styles.snapshotContainer}
+            >
+                <View
+                style={styles.deckInfoContainer}
+                >
+                    <Text style={styles.h1}>
+                        DECK NAME: {deck.name}
+                    </Text>
+                    <Text style={styles.h2}>
+                        CARD COUNT: {cardCount}
+                    </Text>
+                </View>
+                <View
+                
+                >
+                    <Image 
+                        style={styles.card}
+                        source = {{uri: 'https://media.magic.wizards.com/image_legacy_migration/magic/images/mtgcom/fcpics/making/mr224_back.jpg'}} 
+                        />
+                </View>
             </View>
             </Pressable>
         </SafeAreaView>   
@@ -40,29 +49,43 @@ const DeckSnapshot = ({deck, navigation: { navigate }}) => {
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-    //   backgroundColor: '#fff',
       alignItems: 'flex-start',
       justifyContent: 'flex-start',
       marginLeft: 20,
-      marginRight: 20,
+      marginRight: 10,
     },
     h1: {
-        flex: 1,
-        // backgroundColor: 'yellow',
-        alignItems: 'center',
-        justifyContent: 'center',
-        fontSize: 20,
+        // flex: 1,
+        // alignItems: 'center',
+        // justifyContent: 'center',
+        fontSize: 14,
+        fontWeight: 'bold'
     },
     h2: {
-        flex: 1,
-        // backgroundColor: 'blue',
-        alignItems: 'center',
-        justifyContent: 'flex-start',
-        fontSize:15,
+        // flex: 1,
+        // alignItems: 'center',
+        // justifyContent: 'flex-start',
+        fontSize:14,
     },
     outline: {
         borderColor: "red",
         borderWidth: 2,
+    },
+    card: {
+        margin: 20,
+        resizeMode: "contain",
+        width: 194,
+        height: 275,
+    },
+    snapshotContainer:{
+        flexDirection: 'row'
+    }, 
+    deckInfoContainer: {
+        marginLeft: 25,
+        width: 100,
+        paddingTop: 50,
+        alignItems: 'flex-start',
+        justifyContent: 'flex-start',
     }
 });
 
